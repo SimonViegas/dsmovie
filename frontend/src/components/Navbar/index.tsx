59c4e6b029564b3bc1d3171acff4f975c86cbfdf
Simon Viegas <simonviegas@outlook.com.br>
2022-01-11 00:15:22 -0300

    MovieCard

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,4 +1,4 @@
-import { ReactComponent as GithubIcon } from 'assets/imgs/github.svg'
+import { ReactComponent as GithubIcon } from 'assets/img/github.svg'
 import './styles.css'
 
 function Navbar() {
